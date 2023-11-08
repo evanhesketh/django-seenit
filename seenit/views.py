309c98a6a65ede9abc8311cecd440020c3cb19d5
Evan Hesketh <evanhesketh@yahoo.ca>
2023-11-08 16:59:37 -0500

    add home and register views

--- seenit/views.py
+++ seenit/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'seenit/home.html', {})
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "Registration Successful!")
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'seenit/auth/register.html', {'form': form})
