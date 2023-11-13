17759c0b7b497a902b506e8c4e33d0e5fe51cd9f
Evan Hesketh <evanhesketh@yahoo.ca>
2023-11-13 17:24:12 -0500

    update urls for new views

--- seenit/urls.py
+++ seenit/urls.py
@@ -12,7 +12,9 @@ urlpatterns = [
          views.ChannelCreateView.as_view(),
          name="create_channel"),
     path("channels", views.ChannelListView.as_view(), name="channels"),
-    path("channels/<int:channel_id>/new_post",
-         views.PostCreateView.as_view(),
-         name="new_post"),
+    path("channels/<int:pk>",
+         views.ChannelView.as_view(), name="channel-detail"),
+#     path("channels/<int:channel_id>/new_post",
+#          views.PostCreateView.as_view(),
+#          name="new_post"),
 ]
