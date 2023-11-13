a304dfd484b75ec8ec0361f7124e5dd641a4eaaa
Evan Hesketh <evanhesketh@yahoo.ca>
2023-11-13 17:23:31 -0500

    add views for a channel (detail and form)

--- seenit/views.py
+++ seenit/views.py
@@ -1,14 +1,15 @@
-from typing import Any
-from django.db.models.query import QuerySet
-from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.http import HttpResponseForbidden
+from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.views import View
 from django.views.generic.edit import CreateView
-from django.views.generic import ListView
+from django.views.generic.detail import SingleObjectMixin
+from django.views.generic import ListView, DetailView, FormView
 
-from .forms import RegisterForm
-from .models import Channel, Post, Comment
+from .forms import RegisterForm, PostForm
+from .models import User, Channel, Post
 
 
 def home(request):
@@ -48,17 +49,47 @@ class ChannelListView(ListView):
         return Channel.objects.all()
 
 
-class PostCreateView(CreateView):
-    model = Post
-    fields = ["title", "text"]
+class ChannelDetailView(DetailView):
+    model = Channel
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['channel_id'] = self.kwargs['pk']
+        context['form'] = PostForm()
+        return context
+
+
+class ChannelDetailFormView(SingleObjectMixin, FormView):
+    template_name = 'seenit/channel_detail.html'
+    form_class = PostForm
+    model = Channel
+
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden()
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
 
     def form_valid(self, form):
-        user_id = self.request.user.id
-        channel_id = self.kwargs['channel_id']
-        form.instance.channel_id = channel_id
-        form.instance.user_id = user_id
+        title = self.request.POST.get("title")
+        text = self.request.POST.get("text")
+        user = User.objects.get(pk=self.request.user.pk)
+        channel = Channel.objects.get(pk=self.kwargs['pk'])
+        post = Post(title=title, text=text, user_id=user, channel_id=channel)
+        post.save()
         return super().form_valid(form)
 
     def get_success_url(self):
-        channel_id = self.kwargs['channel_id']
-        return reverse(f'seenit:channels/{channel_id}')
+        print("pk=", self.object.pk)
+        return reverse("seenit:channel-detail", kwargs={"pk": self.object.pk})
+
+
+class ChannelView(View):
+    def get(self, request, *args, **kwargs):
+        print("kwargs=", kwargs)
+        view = ChannelDetailView.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = ChannelDetailFormView.as_view()
+        return view(request, *args, **kwargs)
