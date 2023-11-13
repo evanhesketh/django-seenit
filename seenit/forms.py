0d824667fb7d1a9455b80557d3e6b4118f157cd2
Evan Hesketh <evanhesketh@yahoo.ca>
2023-11-13 17:21:45 -0500

    add form for posts

--- seenit/forms.py
+++ seenit/forms.py
@@ -1,8 +1,15 @@
+from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
-from .models import User
+from .models import User, Post
 
 
 class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = UserCreationForm.Meta.fields + ('email',)
+
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ["title", "text"]
