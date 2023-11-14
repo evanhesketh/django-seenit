7d6a569d2bf83e76de011764d1100c05d2ca3d3e
Evan Hesketh <evanhesketh@yahoo.ca>
2023-11-14 13:33:57 -0500

    add views to handle GET/POST reqs to view a post's details and handle comment submission

--- seenit/views.py
+++ seenit/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseForbidden
+from django.http import HttpResponseForbidden, HttpResponseNotFound
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login
@@ -8,8 +8,8 @@ from django.views.generic.edit import CreateView
 from django.views.generic.detail import SingleObjectMixin
 from django.views.generic import ListView, DetailView, FormView
 
-from .forms import RegisterForm, PostForm
-from .models import User, Channel, Post
+from .forms import RegisterForm, PostForm, CommentForm
+from .models import User, Channel, Post, Comment
 
 
 def home(request):
@@ -75,21 +75,72 @@ class ChannelDetailFormView(SingleObjectMixin, FormView):
         text = self.request.POST.get("text")
         user = User.objects.get(pk=self.request.user.pk)
         post = Post(title=title, text=text,
-                    user_id=user, channel_id=self.object)
+                    user=user, channel=self.object)
         post.save()
         return super().form_valid(form)
 
     def get_success_url(self):
-        print("pk=", self.object.pk)
         return reverse("seenit:channel-detail", kwargs={"pk": self.object.pk})
 
 
 class ChannelView(View):
     def get(self, request, *args, **kwargs):
-        print("kwargs=", kwargs)
         view = ChannelDetailView.as_view()
         return view(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
         view = ChannelDetailFormView.as_view()
+        return view(request, *args, **kwargs)
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+    def get(self, request, *args, **kwargs):
+        channel = Channel.objects.get(pk=self.kwargs['channel_id'])
+        if not channel.posts.filter(pk=self.kwargs['pk']):
+            return HttpResponseNotFound()
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post_id'] = self.kwargs['pk']
+        context['form'] = CommentForm()
+        return context
+
+
+class PostDetailFormView(SingleObjectMixin, FormView):
+    template_name = 'seenit/post_detail.html'
+    form_class = CommentForm
+    model = Post
+
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden()
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        text = self.request.POST.get("text")
+        user = User.objects.get(pk=self.request.user.pk)
+        comment = Comment(text=text,
+                          user=user, post=self.object)
+        comment.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("seenit:post-detail",
+                       kwargs={"pk": self.object.pk,
+                               "channel_id": self.kwargs['channel_id']
+                               }
+                       )
+
+
+class PostView(View):
+    def get(self, request, *args, **kwargs):
+        view = PostDetailView.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = PostDetailFormView.as_view()
         return view(request, *args, **kwargs)
