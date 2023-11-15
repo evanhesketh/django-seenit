aa17c8f643889c7352b9bec7229af9c3a28b0f67
Evan Hesketh <evanhesketh@yahoo.ca>
2023-11-15 17:49:14 -0500

    add url for replies

--- seenit/urls.py
+++ seenit/urls.py
@@ -16,6 +16,8 @@ urlpatterns = [
          views.ChannelView.as_view(), name="channel-detail"),
     path("channels/<int:channel_id>/posts/<int:pk>",
          views.PostView.as_view(), name="post-detail"),
+    path("channels/<int:channel_id>/posts/<int:post_id>/comment/<int:pk>",
+         views.handle_reply, name="reply"),
     path("api/v1/upvote", views.upvote, name="upvote"),
     path("api/v1/downvote", views.downvote, name="downvote"),
 ]
