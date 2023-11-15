24f1abf0d95d7bb9280a39ebae0ddb40148444d9
Evan Hesketh <evanhesketh@yahoo.ca>
2023-11-15 17:48:31 -0500

    add reply button click handler to app.js'

--- seenit/static/app.js
+++ seenit/static/app.js
@@ -3,20 +3,20 @@
 const POST_CONTAINERS = document.getElementsByClassName("post-container");
 const UP_VOTE = document.getElementsByClassName("upvote");
 const DOWN_VOTE = document.getElementsByClassName("downvote");
-const BASE_URL = 'http://localhost:8000';
+const REPLY_BTN = document.getElementsByClassName("reply-btn");
+const BASE_URL = "http://localhost:8000";
 
 async function upVote(target) {
   const id = target.getAttribute("data-id");
   const postType = target.getAttribute("data-posttype");
-  const ratingElement = target.nextElementSibling
-  
+  const ratingElement = target.nextElementSibling;
+
   try {
-    await axios.post(`${BASE_URL}/api/v1/upvote`, {"id": id, "type": postType});
+    await axios.post(`${BASE_URL}/api/v1/upvote`, { id: id, type: postType });
     let rating = +ratingElement.innerText;
     rating++;
     ratingElement.textContent = rating;
-
-  } catch(err) {
+  } catch (err) {
     console.log("Error:", err);
   }
 }
@@ -24,33 +24,49 @@ async function upVote(target) {
 async function downVote(target) {
   const id = target.getAttribute("data-id");
   const postType = target.getAttribute("data-posttype");
-  const ratingElement = target.previousElementSibling
-  
+  const ratingElement = target.previousElementSibling;
+
   try {
-    await axios.post(`${BASE_URL}/api/v1/downvote`, {"id": id, "type": postType});
+    await axios.post(`${BASE_URL}/api/v1/downvote`, { id: id, type: postType });
     let rating = +ratingElement.innerText;
     rating--;
     ratingElement.textContent = rating;
-
-  } catch(err) {
+  } catch (err) {
     console.log("Error:", err);
   }
 }
 
-
-window.addEventListener('load', function() {
+function addVoteClickHandlers() {
   for (let elem of POST_CONTAINERS) {
-      elem.addEventListener("click", async function(evt) {
-      const upVoteTarget = evt.target.closest('.upvote');
-      const downVoteTarget = evt.target.closest('.downvote');
-      
+    elem.addEventListener("click", async function (evt) {
+      const upVoteTarget = evt.target.closest(".upvote");
+      const downVoteTarget = evt.target.closest(".downvote");
+
       if (upVoteTarget) {
         upVote(upVoteTarget);
       }
-      
+
       if (downVoteTarget) {
         downVote(downVoteTarget);
       }
     });
   }
+}
+
+function addReplyClickHandler() {
+  for (let elem of REPLY_BTN) {
+    elem.addEventListener("click", function (evt) {
+      const replyForm = evt.target.nextElementSibling;
+      if (replyForm.style.display === "none") {
+        replyForm.style.display = "block";
+      } else {
+        replyForm.style.display = "none";
+      }
+    });
+  }
+}
+
+window.addEventListener("load", function () {
+  addVoteClickHandlers();
+  addReplyClickHandler();
 });
