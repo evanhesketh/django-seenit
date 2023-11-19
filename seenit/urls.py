22ccc7b3509dcfa8a448f30620dc787d426e23ea
Evan Hesketh <evanhesketh@yahoo.ca>
2023-11-19 14:48:10 -0500

    add subscribe/unsubscribe urls

--- seenit/urls.py
+++ seenit/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 
 from . import views
@@ -8,17 +9,19 @@ app_name = "seenit"
 urlpatterns = [
     path("", views.home, name="home"),
     path("register", views.register_user, name="register"),
-    path("users/<int:pk>", views.UserDetailView.as_view(), name="user_detail"),
+    path("users/<int:pk>", login_required(views.UserDetailView.as_view()), name="user_detail"),
     path("create_channel",
-         views.ChannelCreateView.as_view(),
+         login_required(views.ChannelCreateView.as_view()),
          name="create_channel"),
-    path("channels", views.ChannelListView.as_view(), name="channels"),
+    path("channels", login_required(views.ChannelListView.as_view()), name="channels"),
     path("channels/<int:pk>",
-         views.ChannelView.as_view(), name="channel-detail"),
+         login_required(views.ChannelView.as_view()), name="channel-detail"),
     path("channels/<int:channel_id>/posts/<int:pk>",
-         views.PostView.as_view(), name="post-detail"),
+         login_required(views.PostView.as_view()), name="post-detail"),
     path("channels/<int:channel_id>/posts/<int:post_id>/comment/<int:pk>",
          views.handle_reply, name="reply"),
     path("api/v1/upvote", views.upvote, name="upvote"),
     path("api/v1/downvote", views.downvote, name="downvote"),
+    path("users/<int:user_id>/channels/<int:channel_id>/subscribe", views.subscribe, name="subscribe"),
+    path("users/<int:user_id>/channels/<int:channel_id>/unsubscribe", views.unsubscribe, name="unsubscribe"),
 ]
